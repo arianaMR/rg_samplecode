510c9b3981cc119aee80663c13c2a189e6851b08
arianaMR <arianameli13@gmail.com>
2017-03-10 18:11:42 -0800

    commiting new web project to local repo

---
+++ retgrid/retgrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(retgrid.Startup))]
+namespace retgrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
